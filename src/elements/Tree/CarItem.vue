978b3619d7c9e7f802b50e4ffb12f0fe008f5cbb
Nikita Melnikov <nikmel2803@gmail.com>
2018-12-12 23:15:16 +0300

    new layout in tree components

--- src/elements/Tree/CarItem.vue
+++ src/elements/Tree/CarItem.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="tree-item car-item">
     <div class="tree-item__label-wrapper">
-      <span>{{ data.name }} | {{data.model}}</span>
+      <div class="tree-item__dots" @click.capture="edit"></div>
+      <h1 class="car-item__name">{{ data.name }}</h1>
+      <div class="car-item__model">Модель: {{ data.model }}</div>
+      <div class="car-item__description">Описание: {{ data.description }}</div>
+      <div class="car-item__purchase_date">Описание: {{ data.purchase_date }}</div>
     </div>
   </div>
 </template>
@@ -12,10 +16,20 @@
   export default {
     name: 'CarItem',
     mixins: [ItemMixin],
-    props: ['data']
+    props: ['data'],
+    data() {
+      return {
+        name: this.data.name
+      };
+    }
   };
 </script>
 
 <style lang="scss">
-
+  .car-item {
+    &__name {
+      font-size: 20px;
+      margin: 0;
+    }
+  }
 </style>
--- src/elements/Tree/OrganizationItem.vue
+++ src/elements/Tree/OrganizationItem.vue
@@ -1,11 +1,43 @@
 <template>
-  <div class="tree-item">
-    <div class="tree-item__label-wrapper tree-item__label-wrapper--has-children"
-         :class="iconClasses"
-         @click.self="toggleChildren">
-      <span>{{ data.name }}</span>
-      <div class="tree-item__dots" @click.capture="edit"></div>
+  <div class="tree-item organization-item">
+    <div class="tree-item__label-wrapper tree-item__label-wrapper--has-children" :class="iconClasses">
+      <div @click="toggleChildren" v-if="!showChildren">{{ data.name }}</div>
+      <div v-else-if="!isEdit" @click="toggleChildren">
+        <h1 class="organization-item__name">{{ data.name }}</h1>
+        <div class="organization-item__description">Описание: {{ data.description }}</div>
+        <div class="organization-item__founding_date">Дата создания: {{ data.founding_date }}</div>
+        <div class="organization-item__address">Адрес: {{ data.address }}</div>
+      </div>
+      <div v-else>
+        <table>
+          <tr>
+            <td>Название</td>
+            <td><input type="text" v-model="name"></td>
+          </tr>
+          <tr>
+            <td>Описание</td>
+            <td><input type="text" v-model="description"></td>
+          </tr>
+          <tr>
+            <td>Дата основания</td>
+            <td><input type="text" v-model="founding_date"></td>
+          </tr>
+          <tr>
+            <td>Адрес</td>
+            <td><input type="text" v-model="address"></td>
+          </tr>
+          <tr>
+            <td>
+              <button @click="isEdit=false">Отмена</button>
+            </td>
+            <td>
+              <button @click="saveData">Сохранить</button>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
+    <div class="tree-item__dots" @click.capture="edit"></div>
     <car-park-item
       v-if="showChildren"
       :data="data.car_park"
@@ -14,11 +46,6 @@
       v-if="showChildren"
       :data="data.employees"
     ></staff-item>
-    <organization-edit
-      v-if="isEdit"
-      @endEdit="isEdit=false"
-      :data="data"
-    ></organization-edit>
   </div>
 </template>
 
@@ -32,6 +59,10 @@
     name: 'OrganizationItem',
     data() {
       return {
+        name: this.data.name,
+        description: this.data.description,
+        founding_date: this.data.founding_date,
+        address: this.data.address,
         isEdit: false
       };
     },
@@ -40,6 +71,10 @@
     methods: {
       edit() {
         this.isEdit = true;
+        this.showChildren = true;
+      },
+      saveData() {
+
       }
     },
     components: {
@@ -52,6 +87,13 @@
 
 <style lang="scss">
   @import "ItemStyle";
+
+  .organization-item {
+    &__name {
+      font-size: 20px;
+      margin: 0;
+    }
+  }
 
   .car-park-item {
     margin-left: 25px;
